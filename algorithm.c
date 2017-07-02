9328b490daa8cdb8702c244d753ad7d4c5aa5e3f
Jimmy Situ <web@jimmystone.cn>
2017-07-02 10:28:59 +0800

    Fix hash duplicate issue

--- algorithm.c
+++ algorithm.c
@@ -1151,7 +1151,7 @@ static int queue_sia_kernel(int *dev, struct _dev_blk_ctx *blk)
   target = *(uint64_t *)(blk->work->device_target + 24);
   flip80(data, blk->work->data);
 
-  ((uint64_t*)data)[4] = blk->work->blk.nonce;
+  ((uint64_t*)data)[4] = blk->work->blk.nonce + 1;
   // send work (data and target) to tty device
   uint8_t header[3] = {0xAA, 0x00, 0x58};  // Send work command header
   write(*dev, header, 3);

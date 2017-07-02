c5988763c73767b7a7903a338beaab39a1e8007f
Jimmy Situ <web@jimmystone.cn>
2017-07-02 09:55:37 +0800

    Fix hash rate calc bug

--- driver-tty.c
+++ driver-tty.c
@@ -257,7 +257,7 @@ static int64_t tty_scanhash(struct thr_info *thr, struct work *work,
       thrdata->res[thrdata->res[found_idx]] = last_nonce;  // get golden nonce 
       thrdata->res[found_idx]++;
       applog(LOG_DEBUG, "[TTY] tty device found something, nonce: 0x%08x", last_nonce);
-      hashes = last_nonce - first_nonce;
+      hashes = flip32(last_nonce, last_nonce) - first_nonce;
       goto found_nonces;
     }else if(0 == rd){
       applog(LOG_DEBUG, "[TTY] tty device read %d btyes, waiting %d", rd, cnt);
